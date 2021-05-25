505a75521503e859a9c0ac1511adc3fc37842b82
MohammadShoman <mohammad.shoman25@gmail.com>
2021-05-24 23:02:08 -0700

    Q5 pulse check  done

--- react_blog/src/App.js
+++ react_blog/src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // jsx
 const App = () => {
+  const [posts, setPosts] = useState([
+    { userId: 1, id: 150, title: "subject", body: "nothing" },
+    { userId: 2, id: 200, title: "react", body: "nothing" },
+  ]);
+  const newPosts = posts.map((elem, i) => {
+    return (
+      <p key={i}>
+        title:{elem.title}<br/>
+        <span>body:{elem.body}</span>
+      </p>
+    );
+  });
+
   return (
     <>
-      
       <div>
         <h1>Blog App</h1>
       </div>
+      <div>{posts && newPosts}</div>
     </>
   );
 };
