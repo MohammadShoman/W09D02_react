18f84bd375e84642189b585df585e3411b096d0b
MohammadShoman <mohammad.shoman25@gmail.com>
2021-05-24 23:45:38 -0700

    Q6 && Q7 && Q8 pulse check  done

--- react_blog/src/App.js
+++ react_blog/src/App.js
@@ -6,6 +6,11 @@ const App = () => {
     { userId: 1, id: 150, title: "subject", body: "nothing" },
     { userId: 2, id: 200, title: "react", body: "nothing" },
   ]);
+
+  const [userId,setUserId]=useState(1)
+  const [id,setId]=useState(101)
+  const [title,setTitle]=useState("")
+  const [body,setBody]=useState("")
   const newPosts = posts.map((elem, i) => {
     return (
       <p key={i}>
@@ -15,12 +20,30 @@ const App = () => {
     );
   });
 
+const addNewPost=()=>{
+  setPosts([...posts, {userId,id,title,body}])
+}
+
   return (
     <>
       <div>
         <h1>Blog App</h1>
       </div>
-      <div>{posts && newPosts}</div>
+      <div>{posts && newPosts}
+      <button onClick={addNewPost}>OK</button>
+      <input type="text" placeholder="userId" name="userId" onChange={(e)=>{
+        setUserId(e.target.value)
+      }}></input>
+      <input type="text" placeholder="Id" name="Id" onChange={(e)=>{
+        setId(e.target.value)
+      }}></input>
+      <input type="text" placeholder="title" name="title" onChange={(e)=>{
+        setTitle(e.target.value)
+      }}></input>
+      <input type="text" placeholder="body" name="body" onChange={(e)=>{
+        setBody(e.target.value)
+      }}></input>
+      </div>
     </>
   );
 };
